4c70c2500e2e70d2a145b3667390627c9f8aa044
Cody J Powell <codyj.powell@outlook.com>
2020-09-06 15:00:11 -0400

    commit

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,13 @@
 "use strict";
 
 console.log("Hello World from main.js! \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");
+$(document).ready(function () {
+  $('.carousel').slick({
+    slidesToShow: 3,
+    dots: true,
+    centerMode: true
+  });
+});
 var hamburgerButton = document.querySelector('.hamburger');
 var navList = document.querySelector('nav');
 
--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,14 @@ console.log(`Hello World from main.js!
 Change this message, and make sure it changes in the browser 
 to verify that you're working in the right files.`);
 
+$(document).ready(function () {
+	$('.carousel').slick({
+		slidesToShow: 3,
+		dots: true,
+		centerMode: true,
+	});
+});
+
 let hamburgerButton = document.querySelector('.hamburger');
 let navList = document.querySelector('nav');
 let toggleNav = function () {
